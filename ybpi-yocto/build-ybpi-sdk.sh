1feded29bde55c923ca141371ade0957c4fdf019
raphael meyer <raphael.akira.meyer@gmail.com>
2015-07-12 22:32:15 +0200

    Create rpi-build repo

--- ybpi-yocto/build-ybpi-sdk.sh
+++ ybpi-yocto/build-ybpi-sdk.sh
@@ -4,24 +4,28 @@ YOCTO_RELEASE=fido
 
 WORKDIR=/yocto
 
+cd ${WORKDIR}
 if [ ! -d poky ] ; then
   git clone http://git.yoctoproject.org/git/poky
   cd poky && git checkout -b ${YOCTO_RELEASE} origin/${YOCTO_RELEASE}
 fi
-cd ${WORKDIR}
 
+cd ${WORKDIR}
 if [ ! -d meta-raspberrypi ] ; then
-  git clone https://github.com/raphaelmeyer/meta-raspberrypi.git
+  git clone http://git.yoctoproject.org/git/meta-raspberrypi
   cd meta-raspberrypi && git checkout -b ${YOCTO_RELEASE} origin/${YOCTO_RELEASE}
 fi
-cd ${WORKDIR}
 
-cd poky && git pull
 cd ${WORKDIR}
+if [ ! -d rpi-build ] ; then
+  git clone https://github.com/raphaelmeyer/rpi-build.git
+fi
 
-cd meta-raspberrypi && git pull
+cd ${WORKDIR}/poky && git pull
+cd ${WORKDIR}/meta-raspberrypi && git pull
+cd ${WORKDIR}/rpi-build && git pull
+
 cd ${WORKDIR}
-
 source poky/oe-init-build-env rpi-build
 
 bitbake rpi-hwup-image
