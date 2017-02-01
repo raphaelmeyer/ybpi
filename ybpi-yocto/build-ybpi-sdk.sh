22498b58f14498859c6923a6a1ab2f3eb7b3e9b7
raphael meyer <raphael.akira.meyer@gmail.com>
2017-02-01 22:51:02 +0100

    Prepare for morty update

--- ybpi-yocto/build-ybpi-sdk.sh
+++ ybpi-yocto/build-ybpi-sdk.sh
@@ -3,34 +3,43 @@
 YOCTO_RELEASE=morty # 2.2
 
 WORKDIR=/workspace
+YOCTODIR=${WORKDIR}/poky-${YOCTO_RELEASE}
+BUILDDIR=${WORKDIR}/rpi-build
 
-cd ${WORKDIR}
+mkdir -p ${YOCTODIR}
+
+cd ${YOCTODIR}
 if [ ! -d poky ] ; then
   git clone http://git.yoctoproject.org/git/poky
   cd poky && git checkout -b ${YOCTO_RELEASE} origin/${YOCTO_RELEASE}
 fi
 
-cd ${WORKDIR}
+cd ${YOCTODIR}
 if [ ! -d meta-raspberrypi ] ; then
   git clone http://git.yoctoproject.org/git/meta-raspberrypi
   cd meta-raspberrypi && git checkout -b ${YOCTO_RELEASE} origin/${YOCTO_RELEASE}
 fi
 
-cd ${WORKDIR}
+cd ${YOCTODIR}
 if [ ! -d meta-ybpi ] ; then
   git clone https://github.com/raphaelmeyer/meta-ybpi.git
   cd meta-ybpi && git checkout -b ${YOCTO_RELEASE} origin/${YOCTO_RELEASE}
 fi
 
-cd ${WORKDIR}/poky && git pull
-cd ${WORKDIR}/meta-raspberrypi && git pull
-cd ${WORKDIR}/meta-ybpi && git pull
+cd ${YOCTODIR}/poky && git pull
+cd ${YOCTODIR}/meta-raspberrypi && git pull
+cd ${YOCTODIR}/meta-ybpi && git pull
 
-mkdir -p ybpi-build
+mkdir -p ${BUILDDIR}
+source ${YOCTODIR}/poky/oe-init-build-env ${BUILDDIR}
 
-cd ${WORKDIR}
-source poky/oe-init-build-env rpi-build
+bitbake-layers add-layer ${YOCTODIR}/meta-raspberrypi
+bitbake-layers add-layer ${YOCTODIR}/meta-ybpi
 
+#bitbake ybpi-raspberrypi2-image
+#bitbake ybpi-raspberrypi2-image -c populate_sdk
+
+### delete
 #bitbake rpi-hwup-image
 #bitbake rpi-hwup-image -c populate_sdk
 
